f00acdbd53a407d522e7874e272ad1a332b9bc62
adwinadwin <71057412+adwinadwin@users.noreply.github.com>
2020-09-16 16:57:18 +0700

    Update devNet_lab1.py

--- devNet_lab1.py
+++ devNet_lab1.py
@@ -1,4 +1,4 @@
-###################
+################## Lab 1 ###################
 
 name = 'Nguyen Danh Thang'
 
@@ -10,6 +10,7 @@ hocVien2 = {'Ten': 'Hoc vien 2', 'Tuoi': '31', 'Don_vi': 'Viettel' }
 hocVien3 = {'Ten': 'Hoc vien 3', 'Tuoi': '32', 'Don_vi': 'Viettel' }
 hocVien4 = {'Ten': 'Hoc vien 4', 'Tuoi': '33', 'Don_vi': 'Viettel' }
 hocVien5 = {'Ten': 'Hoc vien 5', 'Tuoi': '34', 'Don_vi': 'Viettel' }
+
 danhSach_hocVien = [hocVien1,hocVien2,hocVien3,hocVien4,hocVien5]
 
 print ('-----------------')
@@ -18,4 +19,23 @@ for i in danhSach_hocVien:
     print("Tuoi:" + str(i['Tuoi']))
     print("Don vi:" + i['Don_vi'])
     print ('-----------------')
-    
+
+
+#############################################
+
+x = 15
+if x < 10 :
+    print ('True')
+elif x > 20:
+    print ('False')
+else:
+    print('X nằm giữa 10 và 20')
+	
+	
+	
+
+#############################################
+def hienThi():
+    print ('Tên tôi là Thắng')
+    print ('Tôi năm nay 34 tuổi cmnr')
+hienThi()
