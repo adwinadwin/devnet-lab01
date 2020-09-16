65269982ff6f445466105fd56713b910166d3471
adwinadwin <71057412+adwinadwin@users.noreply.github.com>
2020-09-16 10:15:08 +0700

    Update devNet_lab1.py
    update

--- devNet_lab1.py
+++ devNet_lab1.py
@@ -5,11 +5,17 @@ name = 'Nguyen Danh Thang'
 print ('-----------------')
 print (name)
 
-hoc vien 1 = {'Ten': 'Hoc vien 1', 'Tuoi': '30', 'Don vi': 'Viettel' }
-hoc vien 2 = {'Ten': 'Hoc vien 2', 'Tuoi': '31', 'Don vi': 'Viettel' }
-hoc vien 3 = {'Ten': 'Hoc vien 3', 'Tuoi': '32', 'Don vi': 'Viettel' }
-hoc vien 4 = {'Ten': 'Hoc vien 4', 'Tuoi': '33', 'Don vi': 'Viettel' }
-hoc vien 5 = {'Ten': 'Hoc vien 5', 'Tuoi': '34', 'Don vi': 'Viettel' }
-danhSach_hocVien = {}
+hocVien1 = {'Ten': 'Hoc vien 1', 'Tuoi': '30', 'Don_vi': 'Viettel' }
+hocVien2 = {'Ten': 'Hoc vien 2', 'Tuoi': '31', 'Don_vi': 'Viettel' }
+hocVien3 = {'Ten': 'Hoc vien 3', 'Tuoi': '32', 'Don_vi': 'Viettel' }
+hocVien4 = {'Ten': 'Hoc vien 4', 'Tuoi': '33', 'Don_vi': 'Viettel' }
+hocVien5 = {'Ten': 'Hoc vien 5', 'Tuoi': '34', 'Don_vi': 'Viettel' }
+danhSach_hocVien = [hocVien1,hocVien2,hocVien3,hocVien4,hocVien5]
 
-print (hoc vien 1)
+print ('-----------------')
+for i in danhSach_hocVien:
+    print("Ten:" + i['Ten'])
+    print("Tuoi:" + str(i['Tuoi']))
+    print("Don vi:" + i['Don_vi'])
+    print ('-----------------')
+    
