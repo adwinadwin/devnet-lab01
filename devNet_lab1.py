47129e3e8b8711591a9f67b5dce27b0401a43edd
thangnd <thangnd.set@gmail.com>
2020-09-16 10:09:15 +0700

    Thuc hanh lav Python

---
+++ devNet_lab1.py
@@ -0,0 +1,15 @@
+###################
+
+name = 'Nguyen Danh Thang'
+
+print ('-----------------')
+print (name)
+
+hoc vien 1 = {'Ten': 'Hoc vien 1', 'Tuoi': '30', 'Don vi': 'Viettel' }
+hoc vien 2 = {'Ten': 'Hoc vien 2', 'Tuoi': '31', 'Don vi': 'Viettel' }
+hoc vien 3 = {'Ten': 'Hoc vien 3', 'Tuoi': '32', 'Don vi': 'Viettel' }
+hoc vien 4 = {'Ten': 'Hoc vien 4', 'Tuoi': '33', 'Don vi': 'Viettel' }
+hoc vien 5 = {'Ten': 'Hoc vien 5', 'Tuoi': '34', 'Don vi': 'Viettel' }
+danhSach_hocVien = {}
+
+print (hoc vien 1)
