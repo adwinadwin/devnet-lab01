e0a73ce0e924a0ab4b5e4020a5a79caca2625b8f
adwinadwin <71057412+adwinadwin@users.noreply.github.com>
2020-09-17 15:08:11 +0700

    Add files via upload
    clsss

---
+++ class.py
@@ -0,0 +1,23 @@
+# Có 2 vendor: Cisco và Juniper.
+# - Thiết kế class để có thể tương tác được nó như:
+	# + Hiển thị file cấu hình
+	# + Cấu hình VLAN
+
+class Router(object):
+	def __init__(self, vendor):
+		self.vendor = vendor
+
+class cisco(Router):
+	def config_file(self, config_file, vlan):
+		print('File config cua thiet bi Cisco la: {0}' .format(config_file))
+		print ('Vlan cua thiet bi la:{0}' .format(vlan))
+
+
+class juniper(Router):
+	def config_file(self, config_file, vlan):
+		print('File config cua thiet bi Juniper la: {0}' .format(config_file))
+		print ('Vlan cua thiet bi la:{0}' .format(vlan))
+
+cisco1 = cisco('Cisco')
+cisco1.config_file('Cisco_v1.bin', 100)
+juniper1.config_file('juniper_v1.bin', 200)
